d0479c8c2c05a3de89cb4ad70a712f5342b8be35
Cristiano Ap Lazaro <cristianoalazaro@hotmail.com>
2023-10-14 15:43:38 -0300

    continuing inplementing tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import CrossIcon from "./components/icons/CrossIcon";
+import MoonIcon from "./components/icons/MoonIcon";
 
 const App = () => {
   return (
@@ -7,7 +8,7 @@ const App = () => {
         <header className="container x-auto px-4 pt-8">
           <div className="flex justify-between text-3xl font-bold tracking-[0.3em]">
             <h1 className="uppercase text-white">Todo</h1>
-            <button>luna</button>
+            <button>{<MoonIcon />}</button>
           </div>
           <form className="bg-white rounded-md overflow-hidden py-4 flex gap-2 items-center px-4 mt-8">
             <span className="rounded-full h-5 w-5 inline-block border-2 bg-gray-600"></span>
@@ -19,37 +20,39 @@ const App = () => {
           </form>
         </header>
         <main className="container x-auto px-4 mt-8">
-          <div className="bg-white rounded-md">
-            <article className="flex gap-4 px-4 border-b-gray-300 border-b border-2">
+          <div className="bg-white rounded-md [&>article]:p-4 overflow-hidden">
+            <article className="flex gap-4 border-b-gray-300 border-b border-2">
               <span className="rounded-full h-5 w-5 inline-block border-2 bg-gray-600"></span>
               <p className="text-gray-500 flex-1">complete online javascript course</p>
               <button>{<CrossIcon />}</button>
             </article>
-            <article className="flex gap-4 px-4 border-b-gray-300 border-b border-2">
+            <article className="flex gap-4  border-b-gray-300 border-b border-2">
               <span className="rounded-full h-5 w-5 inline-block border-2 bg-gray-600"></span>
               <p className="text-gray-500 flex-1">complete online javascript course</p>
               <button>{<CrossIcon />}</button>
             </article>
-            <article className="flex gap-4 flex-1 px-4 border-b-gray-300 border-b border-2">
+            <article className="flex gap-4 flex-1  border-b-gray-300 border-b border-2">
               <span className="rounded-full h-5 w-5 inline-block border-2 bg-gray-600"></span>
               <p className="text-gray-500 flex-1">complete online javascript course</p>
               <button>{<CrossIcon />}</button>
             </article>
           </div>
 
-          <section>
-            <span>5 items left</span>
-            <button>Clear completed</button>
+          <section className="px-4 p-4 flex justify-between">
+            <span className="text-gray-400">5 items left</span>
+            <button className="text-gray-400">Clear completed</button>
           </section>
         </main>
 
-        <section className="container mx-auto px-4">
-          <button>All</button>
-          <button>Active</button>
-          <button>Completed</button>
+        <section className="container mt-8 p-4 overflow-hidden">
+          <div className="bg-white p-4 rounded-md flex justify-center gap-4">
+            <button className="text-blue-600">All</button>
+            <button className="hover:text-blue-600">Active</button>
+            <button className="hover:text-blue-600">Completed</button>
+          </div>
         </section>
 
-        <section className="text-center">Drag and drop to reorder list</section>
+        <p className="text-center mt-8">Drag and drop to reorder list</p>
       </div>
     </>
   );
